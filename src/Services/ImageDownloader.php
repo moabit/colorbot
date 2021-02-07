5608ee00eadd738b9eae3c79c0fed66e3dbeb1d6
moabit <orng101@gmail.com>
2021-02-07 12:17:08 +0100

    deleted crop

--- src/Services/ImageDownloader.php
+++ src/Services/ImageDownloader.php
@@ -15,20 +15,15 @@ class ImageDownloader
      * @var string
      */
     protected string $url;
-    /**
-     * @var array
-     */
-    protected array $cropParams;
 
     /**
      * ImageDownloader constructor.
      * @param string $url
-     * @param array $cropParams
+
      */
-    public function __construct(string $url, array $cropParams)
+    public function __construct(string $url)
     {
         $this->url = $url;
-        $this->cropParams = $cropParams;
     }
 
     /**
@@ -39,17 +34,13 @@ class ImageDownloader
         $path = __DIR__ . '/../../storage/src_img.jpeg';
         $output = null;
         $code = null;
-        $res = exec('ffmpeg -ss 00:00:01 -i $(youtube-dl --get-url ' . $this->url . ') -vframes 1 -q:v 2 -y ' . $path, $output, $code);
+        exec('ffmpeg -ss 00:00:01 -i $(youtube-dl --get-url ' . $this->url . ') -vframes 1 -q:v 2 -y ' . $path, $output, $code);
         if ($code !== 0) {
             throw new ImageDownloaderException('Не удалось скачать изображение. Code: ' . $code . 'Output:' . $output);
         }
-       // $image = imagecreatefromjpeg($path);
-      //  $image = imagecrop($image, ['x' => 1000, 'y' => 0, 'width' => 900, 'height' => 800]);
-     //   imagejpeg($image, $path, 100);
         if (!file_exists($path)) {
             throw new ImageDownloaderException('Изображение не найдено');
         }
-
 
     }
 }
--- src/container.php
+++ src/container.php
@@ -8,7 +8,7 @@ return [
     'config' => Util::readJSON(__DIR__ . '/../config.json'),
     ColorService::class => DI\create(ColorService::class),
     ImageDownloader::class => DI\factory(function (ContainerInterface $c) {
-        return new ImageDownloader($c->get('config')['youtube-url'], $c->get('config')['crop-params']);
+        return new ImageDownloader($c->get('config')['youtube-url']);
     }),
     Bot::class => DI\factory(function (ContainerInterface $c) {
         return new Bot($c->get(ColorService::class), $c->get(ImageDownloader::class), $c->get('config')['telegram-api-token'], $c->get('config')['channel-name']);
